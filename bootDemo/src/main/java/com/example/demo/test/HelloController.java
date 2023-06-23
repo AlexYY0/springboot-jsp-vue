69989a0051557c22432bf10f07825f271831eb0a
EmperorWS <1970912595@qq.com>
2023-06-24 01:57:50 +0800

    Initial commit

---
+++ bootDemo/src/main/java/com/example/demo/test/HelloController.java
@@ -0,0 +1,31 @@
+//package com.example.demo.test;
+//
+//import com.example.demo.annotation.ApiVersion;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//
+//@Controller
+//@RequestMapping("/dd")
+//public class HelloController {
+//
+//    @ApiVersion(1)
+//    @RequestMapping("{version}/dd")
+//    @ResponseBody
+//    public String ss() {
+//        return "sssssss";
+//    }
+//
+//    @ApiVersion(2)
+//    @RequestMapping("{version}/dd")
+//    @ResponseBody
+//    public String sss(){
+//        return "ddddddd" + "ssdfd";
+//    }
+//
+//    @RequestMapping("index.html")
+//    public  String index(){
+//        return "index";
+//    }
+//
+//}
